7465af91f896a833778fe1e59f59e9f1def842ed
Timothy Johncox <timothy.johncox@physik.uni-giessen.de>
2026-02-24 11:22:52 +0100

    Remove duplicated nested folder

--- wasserstein-gans-for-realistic-financial-time-series-generation/quantgan/training/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Training module for QuantGAN."""
-
-from quantgan.training.schedule import EpochDecay
-from quantgan.training.trainer import WGANGPTrainer
-
-__all__ = [
-    "EpochDecay",
-    "WGANGPTrainer",
-]
